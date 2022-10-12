052448e858a3c5bdb7f53d55a0d055ce8fa81413
Auriarte20 <uriarte2001alexis@gmail.com>
2022-10-12 12:17:44 -0500

    Add changes necessary

--- api/index.js
+++ api/index.js
@@ -18,11 +18,13 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
+const { getAllGenres } = require('./src/controllers/index.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
+    getAllGenres();
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
