61a146563014fb53c00324122d8504d5e2dfe52d
Auriarte20 <uriarte2001alexis@gmail.com>
2022-10-19 18:51:18 -0500

    Add routes of components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Landing from './components/Landing/Landing';
+import Home from './components/Home/Home';
+import CardDetails from './components/CardDetails/CardDetails';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/home' component={Home} />
+          <Route exact path='/home/:id' component={CardDetails} />
+        </Switch>
+      </div>
   );
 }
 
